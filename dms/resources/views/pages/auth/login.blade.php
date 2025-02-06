c66105753bfd56fdd8a7d95af10f179f912b0049
44-av <arianevillalino@gmail.com>
2025-02-06 14:31:48 +0800

    Ticket Number: DMS-002 Ticket Title: Implement Login Page Description of Change: Done implementing the login page markup

--- dms/resources/views/pages/auth/login.blade.php
+++ dms/resources/views/pages/auth/login.blade.php
@@ -10,19 +10,20 @@
 
 <body>
 <div class="flex min-h-screen">
-    <section class="bg-gray-50 dark:bg-gray-900 flex-1">
+    <section class="bg-[#141414] dark:bg-[#141414] flex-1">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <img class="w-35 h-28 mr-2" src="/img/logo.png" alt="logo">
+            <img class="w-55 h-30 mr-2" src="/img/logo-green.png" alt="logo">
+        <h2 class="text-white text-2xl font-bold">Disease Monitoring System</h2>
             <div class="w-full md:mt-0 sm:max-w-md xl:p-0">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <form class="space-y-4 md:space-y-6" action="#">
                         <div>
                             <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                            <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                            <input type="text" name="username" id="username" class="border bg-none border-[#4caf50] text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-none dark:border-[#4caf50] dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required>
                         </div>
                         <div>
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" placeholder="Password" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                            <input type="password" name="password" id="password" placeholder="Password" class=" bg-none border-[#4caf50] border text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-none dark:border-[#4caf50] dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                         </div>
                         <div class="flex items-center justify-between">
                             <div class="flex items-start">
@@ -37,10 +38,10 @@
             </div>
         </div>
     </section>
-     <div class="bg-[url(img/agriculture-industry.jpg)] flex-1 bg-cover bg-center">
-        <h2 class="text-white text-3xl font-bold absolute right-8">Test System</h2>
-        <p class="text-white absolute right-8">Description</p>
-    </div>
+    <div class="bg-[url(img/agriculture-industry.jpg)] flex-1 bg-cover bg-center flex items-center justify-center relative">
+   
+</div>
+
 </div>
 
 
